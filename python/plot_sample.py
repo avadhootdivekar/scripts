28d002f98bc69a042238db03e5f70c853efb9a4b
Avadhoot Divekar <avadhoot.divekar@gmail.com>
2023-05-11 18:15:41 +0530

    Added 3d surface plot.

--- python/plot_sample.py
+++ python/plot_sample.py
@@ -14,6 +14,7 @@ INVALID_FIELD_VAL	=	0
 X_RANGE = [-10 , 10]
 Y_RANGE = [-10 , 10]
 RESOLUTION = 0.1
+POINT_COUNT = int((X_RANGE[1]-X_RANGE[0])/RESOLUTION)
 
 class obj():
     def __init__(self , position:np.array):
@@ -50,3 +51,16 @@ class P1():
         plt.show()
         return
 
+    def sample_2(self):
+        ax = plt.figure().add_subplot(projection='3d')
+        X = np.arange(X_RANGE[0] , X_RANGE[1] , RESOLUTION )
+        Y = np.arange(Y_RANGE[0] , Y_RANGE[1] , RESOLUTION )
+        X , Y = np.meshgrid(X , Y )
+        Z = X**2 + Y**2
+        log.debug(f"X : {X} , Y : {Y} , Z : {Z}")
+        # for i in range(len(X)):
+        #     for j in range(len(Y)):
+        #         Z[i][j] = i+j
+        ax.plot_surface(X,Y,Z)
+        plt.show()
+        return
