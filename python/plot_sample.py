b7d506875859433ec04440d97544e9fcec6345b4
Avadhoot Divekar <avadhoot.divekar@gmail.com>
2023-05-15 12:12:28 +0530

    WIP.

--- python/plot_sample.py
+++ python/plot_sample.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
+from matplotlib import cm
 import numpy as np
 import math
 import logging
@@ -15,33 +16,65 @@ X_RANGE = [-10 , 10]
 Y_RANGE = [-10 , 10]
 RESOLUTION = 0.1
 POINT_COUNT = int((X_RANGE[1]-X_RANGE[0])/RESOLUTION)
+INIT_OBJ_LIST = [
+    [-9.5,-9.5],
+    [-8,8],
+    [8,-9],
+    [9,9.5]
+]
 
 class obj():
     def __init__(self , position:np.array):
         self.position = position
         return
 
-    def getFAt(self, point):
+    def __str__(self):
+        s = f"OBJ : position: {self.position}"
+        return s
+
+    def getFAt(self, point , default = 0):
         F = INVALID_FIELD_VAL
         d = math.dist(self.position , point)
-        F = d**2
+        if d < 1:
+            F = default
+        else :
+            F = d**(-2)
         log.debug(f"Distance : {d}")
         return F
 
 class P1():
     def __init__(self):
         self.objList = []
+        self.X = np.arange(X_RANGE[0] , X_RANGE[1] , RESOLUTION )
+        self.Y = np.arange(Y_RANGE[0] , Y_RANGE[1] , RESOLUTION )
+        self.X , self.Y = np.meshgrid(self.X , self.Y )
+        self.field = np.zeros((POINT_COUNT , POINT_COUNT) )
         return
 
     def plotField(self):
+        ax = plt.figure().add_subplot(projection='3d')
+        log.debug(f"field : {self.field}")
+        ax.plot_surface(self.X,self.Y,self.field ,  cmap=cm.coolwarm)
+        plt.show()
         return
 
-    def addObj(self, obj):
-        self.objList.append(obj)
+    def initObjList(self):
+        for i in range(len(INIT_OBJ_LIST)):
+            O1 = obj(np.array(INIT_OBJ_LIST[i]) )
+            log.debug(f"Appending obj {O1}")
+            self.objList.append(O1)
+        log.debug(f"Obj List : {self.objList}")
         return
     
     def calculateField(self):
-
+        defaultVal = 0
+        for i in range(len(self.field[0])):
+            for j in range(len(self.field)):
+                self.field[i][j] = 0
+                for eachObj in self.objList:
+                    self.field[i][j] += eachObj.getFAt(np.array([self.X[i][j], self.Y[i][j]]) ,default= defaultVal)
+                defaultVal = self.field[i][j]
+        log.debug(f"Field : {self.field}")
         return
 
     def sample3dPlot(self):
@@ -53,14 +86,15 @@ class P1():
 
     def sample_2(self):
         ax = plt.figure().add_subplot(projection='3d')
-        X = np.arange(X_RANGE[0] , X_RANGE[1] , RESOLUTION )
-        Y = np.arange(Y_RANGE[0] , Y_RANGE[1] , RESOLUTION )
-        X , Y = np.meshgrid(X , Y )
-        Z = X**2 + Y**2
-        log.debug(f"X : {X} , Y : {Y} , Z : {Z}")
+        self.X = np.arange(X_RANGE[0] , X_RANGE[1] , RESOLUTION )
+        self.Y = np.arange(Y_RANGE[0] , Y_RANGE[1] , RESOLUTION )
+        self.X , self.Y = np.meshgrid(X , Y )
+
+        Z = self.X**2 + self.Y**2
+        log.debug(f"X : {self.X} , Y : {self.Y} , Z : {Z}")
         # for i in range(len(X)):
         #     for j in range(len(Y)):
         #         Z[i][j] = i+j
-        ax.plot_surface(X,Y,Z)
+        ax.plot_surface(self.X,self.Y,Z)
         plt.show()
         return
---
+++ python/sample_main.py
@@ -0,0 +1,9 @@
+import plot_sample
+
+p = plot_sample.P1()
+# p.calculateField()
+# p.plotField()
+p.initObjList()
+p.calculateField()
+p.plotField()
+
