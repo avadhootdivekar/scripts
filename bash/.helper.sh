30753928139bc1d21d746e44b2a2794d05e0ed55
avadhootdivekar <46618820+avadhootdivekar@users.noreply.github.com>
2024-11-29 19:08:58 +0530

    Some changes. (#8)
    Co-authored-by: Avadhoot Divekar <avadhoot.divekar@antaris.global>

--- bash/.helper.sh
+++ bash/.helper.sh
@@ -8,16 +8,31 @@ init_profile() {
     export KUBECONFIG=$HOME/.kube/config-$MINIKUBE_PROFILE
     minikube start
 }
-
+rm_container(){
+    __cont=$1
+    docker stop $__cont
+    docker rm $__cont
+}
 use_profile() {
     echo "Arguments : $@"
     export MINIKUBE_PROFILE=$1
     export KUBECONFIG=$HOME/.kube/config-$MINIKUBE_PROFILE
 }
+minikube_setup(){
+    pushd $CODE/PC-playground/
+    ./minikube-setup.sh $@
+    popd
+}
 
+help(){
+    echo "Usage : "
+    echo "init_profile <profile_name> : Create a new minikube profile and use it"
+    echo "use_profile <profile_name> : Use an existing minikube profile"
+}
 main() {
     args=${@:2}
-    echo "Arguments to main : $args"
+    echo "Arguments (2:) to main : $args"
+
     $1 $args
 }
 echo "Script arguments : $@"
