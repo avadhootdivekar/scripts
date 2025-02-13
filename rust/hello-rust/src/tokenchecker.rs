9ea7b6ef58c6f2023449fa47a9fe7cb5c96a4f19
Avadhoot Divekar <avadhoot.divekar@antaris.global>
2025-02-13 19:03:35 +0530

    Build success.

--- rust/hello-rust/src/tokenchecker.rs
+++ rust/hello-rust/src/tokenchecker.rs
@@ -1,25 +1,28 @@
 use std::collections::HashMap;
-use std::time::{Duration, Instance};
-struct checker {
-    map : HashMap<str, Instance>,
+use std::time::{Duration, Instant};
+use std::string::String;
+
+pub struct checker {
+    
+    map : HashMap <String, Instant>,
 }
 
 // Implementation of checker - these are methods on the checker type. 
 impl checker {
-    // fn functionName(argName:argType , argName:argType) -> returnType, returnType {}
-    fn check(me: &self, token: str) -> Duration, bool {
-        if me.map.contains_key(token) {
-            d = me.map[token] - Instance::now();
-            return d, true;
+    // fn functionName(argName:argType , argName:argType) -> (returnType, returnType) {}
+    pub fn check(&mut self, token: String) -> (Duration, bool) {
+        if self.map.contains_key(&token) {
+            let d = self.map[&token] - Instant::now();
+            return (d, true);
         } else {
-            me.map[token] = Instance::now();
-            return Duration::from_millis(0), false;
+            self.map.insert(token, Instant::now());
+            return (Duration::from_millis(0), false);
         }
     }
 
 }
 
-pub fn newChecker() -> &checker {
+pub fn newChecker() -> checker {
     checker {
         map: HashMap::new(),
     }
@@ -37,8 +40,10 @@ mod tests {
     #[test]
     fn test_check() {
         let mut c = newChecker();
-        assert_eq!(c.check("token"), (Duration::from_millis(0), false));
-        assert_eq!(c.check("token"), (Duration::from_millis(0), true));
+        let t = String::from("token");
+        let t2 = String::from("token");
+        assert_eq!(c.check(t), (Duration::from_millis(0), false));
+        assert_eq!(c.check(t2), (Duration::from_millis(0), true));
         println!("test_check completed");
     }
 }
